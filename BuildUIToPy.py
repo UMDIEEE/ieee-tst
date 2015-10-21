989122910f9055f70116c49aa47555a753b68c49
Albert Huang <alberthdev@users.noreply.github.com>
2015-10-21 01:15:23 -0400

    Add tool to automate build of UI files to python

---
+++ BuildUIToPy.py
@@ -0,0 +1,27 @@
+import os
+import sys
+from PyQt4 import uic
+
+ui_to_py = [
+            [ "Setup.ui", "SetupGUI.py" ],
+            [ "Sorting.ui", "SortingGUI.py" ],
+            [ "Details.ui", "DetailsGUI.py" ],
+           ]
+
+for ui2py_pair in ui_to_py:
+    ui_file, py_file = ui2py_pair
+    print("Compiling %s to Python code %s..." % (ui_file, py_file))
+    
+    if os.path.isfile(py_file + "c"):
+        print(" => Removing stale compiled Python code: %s" % (py_file + "c"))
+        os.remove(py_file + "c")
+    
+    try:
+        py_file_fh = open(py_file, "w")
+    except IOError:
+        print("ERROR: Could not open %s for writing!" % py_file)
+        sys.exit(1)
+    
+    uic.compileUi(ui_file, py_file_fh)
+    
+    py_file_fh.close()
